1cbc6264d4430ec35398575360c2fe010da2ef20
Fiquegnima <K.schaefer92@gmx.de>
2023-06-14 00:38:31 +0200

    bff open id connect template with modal

---
+++ Test/Client/GlobalUsings.cs
@@ -0,0 +1,15 @@
+﻿global using System.Net;
+global using System.Net.Http.Headers;
+global using System.Net.Http.Json;
+global using System.Security.Claims;
+
+global using Test.Client;
+global using Test.Client.Services;
+global using Test.Shared.Authorization;
+global using Test.Shared.Defaults;
+
+global using Microsoft.AspNetCore.Components;
+global using Microsoft.AspNetCore.Components.Authorization;
+global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+global using Microsoft.Extensions.DependencyInjection.Extensions;
+global using Microsoft.JSInterop;
